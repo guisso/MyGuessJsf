ec37f7e4b630511121c2dd1b11dae6c9cfd2853e
guisso <lguisso@gmail.com>
2021-03-18 15:36:51 -0300

    Language specified by user

--- src/main/java/io/github/guisso/jakartaee8/myguessjsf/UserNumberBean.java
+++ src/main/java/io/github/guisso/jakartaee8/myguessjsf/UserNumberBean.java
@@ -8,11 +8,10 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 /**
- * A session CDI managed bean that controls the business
- * logic of the application: draws a random number in a range
- * and generates actions and responses based on the user's 
- * guesses.
- * 
+ * A session CDI managed bean that controls the business logic of the
+ * application: draws a random number in a range and generates actions and
+ * responses based on the user's guesses.
+ *
  * @author Luis Guisso <luis.guisso at ifnmg.edu.br>
  */
 @Named
@@ -23,8 +22,15 @@ public class UserNumberBean implements Serializable {
     private final Integer minimum;
     private final Integer maximum;
     private Integer guess;
+    
+    // Language specified by user
+    private String lang;
 
     public UserNumberBean() {
+        
+        // Default language
+        lang = "pt-BR";
+
         minimum = 1;
         maximum = 10;
         random = (int) (Math.random()
@@ -32,7 +38,7 @@ public class UserNumberBean implements Serializable {
                 + minimum;
         System.out.println(">> Sorteado: " + random);
     }
-    
+
     //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
     public Integer getRandom() {
         return random;
@@ -53,26 +59,43 @@ public class UserNumberBean implements Serializable {
     public void setGuess(Integer guess) {
         this.guess = guess;
     }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
     //</editor-fold>
 
     public String getResponse() {
         // Recupera contexto da aplicação
         FacesContext context = FacesContext.getCurrentInstance();
         Application application = context.getApplication();
-        
+
         // Recupera fonte de recursos para internacionalização
         ResourceBundle resourceBundle = application.getResourceBundle(context, "i18n");
 
         if (random.equals(guess)) {
             // Destrói sessão
             FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-            
+
             // Recupera e devolve resposta de sucesso
             return resourceBundle.getString("response.success");
         } else {
-            
+
             // Recupera e devolve resposta de falha
             return resourceBundle.getString("response.fail");
         }
     }
+
+    public String defineLang(String lang) {
+        this.setLang(lang);
+
+        // Null return refreshes the page
+        // without navigation to another destination
+        return null;
+    }
+
 }
